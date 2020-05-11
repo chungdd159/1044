a1e8f4f66a64305fb1cd1b740ff968a5cb0ca7c3
chungdd <dchung1509@gmail.com>
2020-05-11 22:51:25 +0700

    Updates

---
+++ precache-manifest.c03834380cc833890c13116ee5939671.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8ac4967a4b3cc565ed328ae55fe9a6c",
+    "url": "/1044/index.html"
+  },
+  {
+    "revision": "ae1a899e9059cdecb59e",
+    "url": "/1044/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "86f9aaffc96d142e158e",
+    "url": "/1044/static/js/2.20ffca34.chunk.js"
+  },
+  {
+    "revision": "005c97f82cee60e20eac659314ea29ec",
+    "url": "/1044/static/js/2.20ffca34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae1a899e9059cdecb59e",
+    "url": "/1044/static/js/main.e566e910.chunk.js"
+  },
+  {
+    "revision": "0fb8c7913ebc0ba030a9",
+    "url": "/1044/static/js/runtime-main.3f65dca4.js"
+  }
+]);
