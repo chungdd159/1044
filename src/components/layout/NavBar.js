d64112d63ee76642708f4faa900adb92a4ec2578
chungdd <dchung1509@gmail.com>
2020-05-11 23:04:25 +0700

    a

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <nav className="navbar navbar-dark bg-dark mb-5">
-      <span className="navbar-brand mb-0 h1 mx-auto">Lyrics Finder</span>
+    <nav className="navbar navbar-dark bg-dark mb-5 text-center">
+      <Link to="/" className="mx-auto">
+        <span className="navbar-brand mb-0 h1">Lyrics Finder</span>
+      </Link>
     </nav>
   );
 };
